5d371a15fc1646e95a8e5612b7c00bc8353ac9b0
WaleedOmar87 <waleedomar87@gmail.com>
2023-03-18 20:32:02 +0200

    Updated theme

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -124,6 +124,12 @@ _G.packer_plugins = {
     path = "/Users/waleedomar/.local/share/nvim/site/pack/packer/start/mason.nvim",
     url = "https://github.com/williamboman/mason.nvim"
   },
+  moonfly = {
+    config = { "\27LJ\2\n7\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0\24colorscheme moonfly\bcmd\bvim\0" },
+    loaded = true,
+    path = "/Users/waleedomar/.local/share/nvim/site/pack/packer/start/moonfly",
+    url = "https://github.com/bluz71/vim-moonfly-colors"
+  },
   ["nvim-cmp"] = {
     loaded = true,
     path = "/Users/waleedomar/.local/share/nvim/site/pack/packer/start/nvim-cmp",
@@ -159,12 +165,6 @@ _G.packer_plugins = {
     path = "/Users/waleedomar/.local/share/nvim/site/pack/packer/start/plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
   },
-  ["rose-pine"] = {
-    config = { "\27LJ\2\n9\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0\26colorscheme rose-pine\bcmd\bvim\0" },
-    loaded = true,
-    path = "/Users/waleedomar/.local/share/nvim/site/pack/packer/start/rose-pine",
-    url = "https://github.com/rose-pine/neovim"
-  },
   ["telescope.nvim"] = {
     loaded = true,
     path = "/Users/waleedomar/.local/share/nvim/site/pack/packer/start/telescope.nvim",
@@ -173,10 +173,10 @@ _G.packer_plugins = {
 }
 
 time([[Defining packer_plugins]], false)
--- Config for: rose-pine
-time([[Config for rose-pine]], true)
-try_loadstring("\27LJ\2\n9\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0\26colorscheme rose-pine\bcmd\bvim\0", "config", "rose-pine")
-time([[Config for rose-pine]], false)
+-- Config for: moonfly
+time([[Config for moonfly]], true)
+try_loadstring("\27LJ\2\n7\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0\24colorscheme moonfly\bcmd\bvim\0", "config", "moonfly")
+time([[Config for moonfly]], false)
 
 _G._packer.inside_compile = false
 if _G._packer.needs_bufread == true then
