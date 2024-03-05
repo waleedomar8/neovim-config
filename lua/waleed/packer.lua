7878cbb9e1e64fdd7d9e307b565f9e1596726038
WaleedOmar87 <waleedomar87@gmail.com>
2024-03-05 20:21:57 +0200

    Updated packages

--- lua/waleed/packer.lua
+++ lua/waleed/packer.lua
@@ -19,8 +19,6 @@ return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
-  use 'othree/html5.vim'
-
   use {
 	  'nvim-telescope/telescope.nvim', tag = '0.1.1',
 	  -- or                            , branch = '0.1.x',
@@ -98,7 +96,6 @@ return require('packer').startup(function(use)
         require('Comment').setup()
     end
   }
-  use('github/copilot.vim')
   use {'romgrk/barbar.nvim', requires = 'nvim-web-devicons'}
   use("xiyaowong/transparent.nvim")
   use('aca/emmet-ls')
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -120,11 +120,6 @@ _G.packer_plugins = {
     path = "/Users/waleedomar/.local/share/nvim/site/pack/packer/start/cmp_luasnip",
     url = "https://github.com/saadparwaiz1/cmp_luasnip"
   },
-  ["copilot.vim"] = {
-    loaded = true,
-    path = "/Users/waleedomar/.local/share/nvim/site/pack/packer/start/copilot.vim",
-    url = "https://github.com/github/copilot.vim"
-  },
   ["emmet-ls"] = {
     loaded = true,
     path = "/Users/waleedomar/.local/share/nvim/site/pack/packer/start/emmet-ls",
@@ -268,11 +263,6 @@ _G.packer_plugins = {
     path = "/Users/waleedomar/.local/share/nvim/site/pack/packer/start/trouble.nvim",
     url = "https://github.com/folke/trouble.nvim"
   },
-  ["twig.vim"] = {
-    loaded = true,
-    path = "/Users/waleedomar/.local/share/nvim/site/pack/packer/start/twig.vim",
-    url = "https://github.com/nelsyeung/twig.vim"
-  },
   ["vim-eldar"] = {
     loaded = true,
     path = "/Users/waleedomar/.local/share/nvim/site/pack/packer/start/vim-eldar",
@@ -291,22 +281,22 @@ _G.packer_plugins = {
 }
 
 time([[Defining packer_plugins]], false)
--- Config for: nvim-autopairs
-time([[Config for nvim-autopairs]], true)
-try_loadstring("\27LJ\2\n@\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\19nvim-autopairs\frequire\0", "config", "nvim-autopairs")
-time([[Config for nvim-autopairs]], false)
--- Config for: trouble.nvim
-time([[Config for trouble.nvim]], true)
-try_loadstring("\27LJ\2\ni\0\0\3\0\4\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0B\0\2\1K\0\1\0\1\0\3\rposition\vbotton\25use_diagnostic_signs\2\nicons\2\nsetup\ftrouble\frequire\0", "config", "trouble.nvim")
-time([[Config for trouble.nvim]], false)
 -- Config for: Comment.nvim
 time([[Config for Comment.nvim]], true)
 try_loadstring("\27LJ\2\n5\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\fComment\frequire\0", "config", "Comment.nvim")
 time([[Config for Comment.nvim]], false)
+-- Config for: nvim-autopairs
+time([[Config for nvim-autopairs]], true)
+try_loadstring("\27LJ\2\n@\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\19nvim-autopairs\frequire\0", "config", "nvim-autopairs")
+time([[Config for nvim-autopairs]], false)
 -- Config for: gitsigns.nvim
 time([[Config for gitsigns.nvim]], true)
 try_loadstring("\27LJ\2\n6\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\rgitsigns\frequire\0", "config", "gitsigns.nvim")
 time([[Config for gitsigns.nvim]], false)
+-- Config for: trouble.nvim
+time([[Config for trouble.nvim]], true)
+try_loadstring("\27LJ\2\ni\0\0\3\0\4\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0B\0\2\1K\0\1\0\1\0\3\rposition\vbotton\25use_diagnostic_signs\2\nicons\2\nsetup\ftrouble\frequire\0", "config", "trouble.nvim")
+time([[Config for trouble.nvim]], false)
 
 _G._packer.inside_compile = false
 if _G._packer.needs_bufread == true then
